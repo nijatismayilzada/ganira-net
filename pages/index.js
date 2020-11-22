540790bf0d9ed083cdcec7a2d2837e32de044775
Nijat Ismayilzada <ismayilzadan@gmail.com>
2020-11-22 16:48:21 +0000

    NEXT.js tutorial changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Link from "next/link";
+import Link from 'next/link'
+import Date from '../components/date'
+import {getSortedPostsData} from "../lib/posts";
 
-export default function Home() {
+export default function Home({allPostsData}) {
     return (
         <Layout home>
             <Head>
@@ -17,7 +19,32 @@ export default function Home() {
                 </p>
             </section>
 
-            <Link href='posts/first-post'><a> First post </a></Link>
+
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({id, date, title}) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br/>
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date}/>
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
